b75d59a4f882518a79b08411c69d10eaa64e1691
arthurcgc <arthur.cavalcante.puc@gmail.com>
2018-07-09 19:35:04 -0300

    first commit, adding a script that reads a line and finds a brazilian phone number in it with country and area code if they exist

---
+++ README.md
@@ -0,0 +1,1 @@
+Just a bunch of scripts in python where I aim to automate some things and learn while doing it.
---
+++ phone_number.py
@@ -0,0 +1,13 @@
+import re
+
+def getPhoneNumber(string):
+    phoneNum = re.compile(r"(\+\d{2} )?(\d{2} )?(\d{4}-\d{4})")
+    mo = phoneNum.search(string)
+    return mo
+
+string = input("Type Line:\n")
+number = getPhoneNumber(string)
+
+print("country code: {}".format(number.group(1)))
+print("area code: {}".format(number.group(2)))
+print("phone number: {}".format(number.group(3)))
