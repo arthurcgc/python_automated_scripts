72a78fa38677d7a5cfcc26d52f8fd9427a7d79a0
arthurcgc <arthur.cavalcante.puc@gmail.com>
2018-07-10 22:06:07 -0300

    perfected the output strings of the brazilian phone numbers

--- email_and_phone_regex/email_phone_regex.py
+++ email_and_phone_regex/email_phone_regex.py
@@ -1,17 +1,27 @@
 import pyperclip
 import re
 
+"""
+
+Python3 script that takes a string from the clipboard
+with the contents of brazilians phone numbers
+and email addresses, filters them, prints them on the
+terminal and copies to clipboard.
+
+"""
+
 
 def getPhoneNumber(string):
     phoneRegex = re.compile(r"""(
-    (\+\d{2})?                  # country code
-    (\s|\.)?                    # separator
-    (\d{2})?                    # area code
-    (\s|\.)?                    # separator
-    \d{4}                       # first 4 digits
-    (\s|-|\.)?                   # separator
-    \d{4}                       # last 4 digits
-    )""", re.VERBOSE)
+      (\+\d{2})?                  # country code
+      (\s|\.)?                    # separator
+      (\d{2})?                    # area code
+      (\s|\.)?                    # separator
+      ([9])?                      # 9 upfront, for cell numbers
+      (\d{4})                       # first 4 digits
+      (\s|-|\.)?                   # separator
+      (\d{4})                       # last 4 digits
+      )""", re.VERBOSE)
     match_list = phoneRegex.findall(string)
     return match_list
 
@@ -32,7 +42,17 @@ matches = []
 phones = getPhoneNumber(text)
 emails = getEmail(text)
 for groups in phones:
-    matches.append(groups[0])
+    if groups[5] != "":
+        phoneNum = " ".join(
+            [groups[1], groups[3]]
+        )
+
+        phoneNum += "".join([" ", groups[5], groups[6], "-", groups[8]])
+    else:
+        phoneNum = " ".join([groups[1], groups[3]])
+        phoneNum += "".join([" ", groups[6], "-", groups[8]])
+    matches.append(phoneNum)
+
 for groups in emails:
     matches.append(groups[0])
 
